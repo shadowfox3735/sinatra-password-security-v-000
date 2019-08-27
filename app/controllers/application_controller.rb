21ffdbcd4cb50f749aaab145ea8c6411d1f38f6d
shadowfox3735 <normanpan180@gmail.com>
2019-08-27 23:49:23 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,6 +18,8 @@ class ApplicationController < Sinatra::Base
 
 	post "/signup" do
 		user = User.new(:username => params[:username], :password => params[:password])
+		
+		
 	end
 
 	get "/login" do
