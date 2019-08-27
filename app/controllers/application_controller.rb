c7a967ca5c01e4adb3eed333987c19997d0f9317
shadowfox3735 <normanpan180@gmail.com>
2019-08-27 23:51:07 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,12 +27,16 @@ class ApplicationController < Sinatra::Base
 	end
 
 	get "/login" do
-	  user = User.find_by(:username => params[:username])
 		erb :login
 	end
 
 	post "/login" do
-		#your code here!
+	  user = User.find_by(:username => params[:username])
+		if user
+      redirect "/success"
+    else
+      redirect "/failure"
+    end
 	end
 
 	get "/success" do
